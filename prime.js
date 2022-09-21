4fb201c41bce168c937ddc0681233aff26f874f6
8309h <harshalwagh201718@gmail.com>
2022-09-21 22:49:11 +0530

    check prime

---
+++ prime.js
@@ -0,0 +1,16 @@
+/// program for check  whether the given number is prime or not.
+
+function checkprime(num){
+
+  
+  for(let i=0;i<=num;i++){
+     if(num%i==0){
+      
+    }
+      }if(factors ==2){
+      console.log(num,"is a prime");
+    }else{
+      console.log(,"is not prime");
+}
+}
+checkprime(120); 
